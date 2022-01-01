f5dcf5cc233bdfab2068feebb4ac799bf0670070
abnerchiu <boloage@gmail.com>
2022-01-02 01:17:51 +0800

    allow all cors origins

--- src/main/kotlin/com/todolist/configuration/WebConfiguration.kt
+++ src/main/kotlin/com/todolist/configuration/WebConfiguration.kt
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class WebConfiguration: WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5504")
+                .allowedOrigins("*")
                 .allowedMethods("*")
                 .allowedHeaders("*")
     }
